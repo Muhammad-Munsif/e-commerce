ede181621eb2f83566e239c1aea00836fdeffc42
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-16 22:24:53 +0500

    Small changes in a few component like cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,121 +9,59 @@ import ProductCard from "../Components/ProductCard";
 import Shop from "./Shop";
 
 const Home = () => {
-// const mockData =[
-//     {
-//         id : 1,
-//         image :shoes,
-//         name : "product 1",
-//         price : 23.54
-//     },
-//     {
-//         id : 2,
-//         image :watch,
-//         name : "product 2",
-//         price : 40.54
-//     },
-//     {
-//         id : 3,
-//         image :shirt,
-//         name : "product 3",
-//         price : 35.12
-//     },
-//     {
-//         id : 4,
-//         image :bluetooth,
-//         name : "product 4",
-//         price : 20.25
-//     },
-//     {
-//         id : 5,
-//         image :gold,
-//         name : "product 5",
-//         price : 40.54
-//     },
-//     {
-//         id : 6,
-//         image :shoes,
-//         name : "product 6",
-//         price : 2
-//     },
-//     {
-//         id : 7,
-//         image :jewellary,
-//         name : "product 7",
-//         price : 54.20
-//     },
-//     {
-//         id : 8,
-//         image :gold,
-//         name : "product 8",
-//         price : 75.45
-//     },
-//     {
-//         id : 9,
-//         image :shirt,
-//         name : "product 9",
-//         price : 23.54
-//     },
-//     {
-//         id : 10,
-//         image :shoes,
-//         name : "product 10",
-//         price : 50.54
-//     },
-// ]
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.product);
 
-  const dispatch = useDispatch()
-  const products = useSelector(state=> state.product)
-
-  useEffect(()=>{
-    dispatch(setProducts(mockData))
-  }, [])
+  useEffect(() => {
+    dispatch(setProducts(mockData));
+  }, []);
   return (
     <div>
-    <div className="bg-white ">
-      <div className="container mx-auto mt-2 px-4 md:px-16 lg:px-24 py-4 flex flex-col md:flex-row space-x-2">
-        <div className="w-full h-full md:w-3/12 ">
-          <div className="bg-red-600 text-xs px-2 py-2.5 font-bold text-white">
-            Shop by Categories
+      <div className="bg-white ">
+        <div className="container mx-auto mt-2 px-4 md:px-16 lg:px-24 py-4 flex flex-col md:flex-row space-x-2">
+          <div className="w-full h-full md:w-3/12 ">
+            <div className="bg-red-600 text-xs px-2 py-2.5 font-bold text-white">
+              Shop by Categories
+            </div>
+            <ul className="space-y-4 bg-gray-100 p-3 border-1 border-gray-300 h-[350px]">
+              {Categories.map((category, index) => (
+                <li
+                  key={index}
+                  className="flex items-center font-medium text-sm"
+                >
+                  <div className="w-2 h-2 border border-red-500 mr-2"></div>
+                  {category}
+                </li>
+              ))}
+            </ul>
           </div>
-          <ul className="space-y-4 bg-gray-100 p-3 border-1 border-gray-300 h-[350px]">
-            {Categories.map((category, index) => (
-              <li key={index} className="flex items-center font-medium text-sm">
-                <div className="w-2 h-2 border border-red-500 mr-2"></div>
-                {category}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="w-full md:w-9/12 mt-8 md:mt-0 h-96 relative rounded-lg">
-          <img src={Person} alt="not found" className="w-full h-full" />
-          <div className="absolute top-32 left-8">
-            <p className="text-gray-600 mb-4">code with Munsif</p>
-            <h2 className="text-3xl font-bold">Welcome to EasyShop</h2>
-            <p className="text-xl mt-2.5 text-gray-800 font-bold">
-              Million + Products
-            </p>
-            <button className="text-white bg-red-600 hover:bg-red-700 mt-2.5 px-8 py-1.5 transform transition-transform duration-300 hover:scale-105">
-              SHOP NOW
-            </button>
+          <div className="w-full md:w-9/12 mt-8 md:mt-0 h-96 relative rounded-lg">
+            <img src={Person} alt="not found" className="w-full h-full" />
+            <div className="absolute top-32 left-8">
+              <p className="text-gray-600 mb-4">code with Munsif</p>
+              <h2 className="text-3xl font-bold">Welcome to EasyShop</h2>
+              <p className="text-xl mt-2.5 text-gray-800 font-bold">
+                Million + Products
+              </p>
+              <button className="text-white bg-red-600 hover:bg-red-700 mt-2.5 px-8 py-1.5 transform transition-transform duration-300 hover:scale-105">
+                SHOP NOW
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <InfoSection />
-      <CategorySection />
+        <InfoSection />
+        <CategorySection />
 
-      <div className="container mx-auto py-12 px-4 md:px-16 lg:px-24 ">
-        <h2 className="text-2xl font-bold mb-6 text-center">Top Products</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
-          {
-            products.products.slice(0, 5).map(((product, index) =>(
-              <ProductCard product={product}  key={index}/>
-            )))
-          }
+        <div className="container mx-auto py-12 px-4 md:px-16 lg:px-24 ">
+          <h2 className="text-2xl font-bold mb-6 text-center">Top Products</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
+            {products.products.slice(0, 5).map((product, index) => (
+              <ProductCard product={product} key={index} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-    <Shop />
+      <Shop />
     </div>
   );
 };

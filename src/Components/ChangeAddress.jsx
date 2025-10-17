2e5f1b02c4bd99131fbec2ba80b08ce20b47fba2
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-17 22:20:35 +0500

    Small changes in a ChangeAddress component

--- src/Components/ChangeAddress.jsx
+++ src/Components/ChangeAddress.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const ChangeAddress = ({setAddress, setIsModelOpen}) => {
-    const [newAddress, setNewAddress] = useState("")
-    const onCloseModel = () =>{
-        setAddress(newAddress)
-        setIsModelOpen(false)
-    }
+const ChangeAddress = ({ setAddress, setIsModelOpen }) => {
+  const [newAddress, setNewAddress] = useState("");
+  const onCloseModel = () => {
+    setAddress(newAddress);
+    setIsModelOpen(false);
+  };
   return (
     <div>
-      <input onChange={(e) =>setNewAddress(e.target.value)} type="text" placeholder='Enter New Address' className='border p-2 w-full mb-4'/>
-      <div className='flex justify-end  text-white'>
-        <button className='bg-gray-500 rounded px-4 py-2 mr-2' onClick={() =>setIsModelOpen(false)}>Cancel</button>
-        <button className='bg-blue-500 rounded px-4 py-2' onClick={onCloseModel}>Save Address</button>
-        </div>
+      <input
+        onChange={(e) => setNewAddress(e.target.value)}
+        type="text"
+        placeholder="Enter New Address"
+        className="border p-2 w-full mb-4"
+      />
+      <div className="flex justify-end  text-white">
+        <button
+          className="bg-gray-500 rounded px-4 py-2 mr-2"
+          onClick={() => setIsModelOpen(false)}
+        >
+          Cancel
+        </button>
+        <button
+          className="bg-blue-500 rounded px-4 py-2"
+          onClick={onCloseModel}
+        >
+          Save Address
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChangeAddress
+export default ChangeAddress;

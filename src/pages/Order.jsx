c76ca233512b02713a4374d3c8195667988c7554
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-19 23:45:52 +0500

    a few changes in checkout component

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Order = () => {
+const Order = ({ order }) => {
   return (
     <div>
-        <h2>Thank you for your order</h2>
-        <p>Your order has ben placed successfully you will receive an email comfirmation shortly</p>
-        <div>
-            <h3>Order Number</h3>
-        </div>
+      <h2>Thank you for your order</h2>
+      <p>
+        Your order has ben placed successfully you will receive an email
+        comfirmation shortly
+      </p>
+      <div>
+        <h3>Order Summary</h3>
+        <p>Order Number: {}</p>
+        <p>shippingInformation: {}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;

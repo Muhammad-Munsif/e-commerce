c34703fde208f56e0aefcdbab180ab76ba73bb5a
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-19 23:46:18 +0500

    a few changes in checkout component

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,36 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import Home from './pages/Home';
-import Shop from './pages/Shop';
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Order from "./pages/Order";
 
-
 function App() {
-  const [order, setOrder] = useState(null)
+  const [order, setOrder] = useState(null);
   return (
-      <Router>
-        <div className="">
-          <Navbar />
-          <main className="">
-            <Routes>
-              <Route path="/*" element={<Home />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/checkout" element={<Checkout  setOrder={setOrder}/>} />
-              <Route path="/order-confirmation" element={<Order order={order}/>} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+    <Router>
+      <div className="">
+        <Navbar />
+        <main className="">
+          <Routes>
+            <Route path="/*" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/checkout"
+              element={<Checkout setOrder={setOrder} />}
+            />
+            <Route
+              path="/order-confirmation"
+              element={<Order order={order} />}
+            />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

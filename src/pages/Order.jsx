9450b0bac266e0ec9317bcaf02b3cdd609beb014
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-20 15:07:54 +0500

    Make a few changes in  a Register, login and navbar component

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Order = ({ order }) => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="container mx-auto py-8 px-4 md:px-16 lg:px-24">
@@ -24,22 +23,31 @@ const Order = ({ order }) => {
         </div>
         <div className="mt-4 ">
           <h3 className="text-md font-semibold mb-2">Porducts Ordered</h3>
-          {
-            order.products.map((product) => (
-              <div key={product.id} className="flex justify-center mt-2">
-                <p>{product.name} (x{product.quantity})</p>
-                <p>{product.price} * {product.quantity}</p>
-              </div>
-            ))
-          }
+          {order.products.map((product) => (
+            <div key={product.id} className="flex justify-center mt-2">
+              <p>
+                {product.name} (x{product.quantity})
+              </p>
+              <p>
+                {product.price} * {product.quantity}
+              </p>
+            </div>
+          ))}
         </div>
         <div className="mt-4 flex justify-between">
           <span>Total Price</span>
           <span className="font-semibold">{order.totalPrice.toFixed(2)}</span>
         </div>
         <div className="mt-6">
-          <button className="bg-green-500 py-2 px-4 text-white hover:bg-green-600">Order Tracking</button>
-          <button className="bg-red-500 py-2 px-4 text-white hover:bg-red-800" onClick={() => navigate('/')}>Continue Shipping</button>
+          <button className="bg-green-500 py-2 px-4 text-white hover:bg-green-600">
+            Order Tracking
+          </button>
+          <button
+            className="bg-red-500 py-2 px-4 text-white hover:bg-red-800"
+            onClick={() => navigate("/")}
+          >
+            Continue Shipping
+          </button>
         </div>
       </div>
     </div>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,47 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { FaCarSide, FaQuestion } from 'react-icons/fa'
-import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { FaCarSide, FaQuestion } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-    const {id} = useParams()
-    const products = useSelector((state) => state.product.products)
-    const [product, setProduct] = useState()
+  const { id } = useParams();
+  const products = useSelector((state) => state.product.products);
+  const [product, setProduct] = useState();
 
+  useEffect(() => {
+    const newProduct = products.find((product) => product.id === parseInt(id));
+    setProduct(newProduct);
+  }, [id]);
 
-    useEffect(()=>{
-        const newProduct = products.find(product => product.id === parseInt(id))
-        setProduct(newProduct)
-    }, [id])
-
-    if(!product) return <div>Loading...</div>;
+  if (!product) return <div>Loading...</div>;
   return (
-    <div className='container mx-auto py-8 px-4 md:px-16 lg:px-24'>
-      <div className='flex flex-col md:flex-row gap-x-16'>
+    <div className="container mx-auto py-8 px-4 md:px-16 lg:px-24">
+      <div className="flex flex-col md:flex-row gap-x-16">
         {/* Product Image detail  */}
-        <div className='flex justify-center md:w-1/2 shadow-md md:px-8 h-96'>
-            <img src={product.image} alt={product.name}  className='h-full'/>
+        <div className="flex justify-center md:w-1/2 shadow-md md:px-8 h-96">
+          <img src={product.image} alt={product.name} className="h-full" />
         </div>
-        <div className='flex flex-col items-center gap-y-2 p-4 md:w-1/2 shadow-md md:p-16 h-96'>
-            <h2 className='text-3xl font-semibold mb-2'>{product.name}</h2>
-            <p className='text-xl font-semibold text-gray-800 mb-4'>{product.price}</p>
-            <div className='flex items-center gap-x-2'>
-                {/* <label htmlFor="quantity" className='mr-2'>Quantity:</label> */}
-                <input type="number" id='quantity' className='border-1 border-gray-200 w-16' />
-                <button className='bg-red-600 py-1.5 px-4 rounded hover:bg-red-800 text-white'>Add to cart</button>
-            </div>
-            <div className='flex flex-col gap-y-4 mt-4'>
-                <p className='flex items-center'><FaCarSide className='mr-1'/> Delivery & Return</p>
-                <p className='flex items-center'><FaQuestion className='mr-1'/>Ask a Question</p>
-            </div>
+        <div className="flex flex-col items-center gap-y-2 p-4 md:w-1/2 shadow-md md:p-16 h-96">
+          <h2 className="text-3xl font-semibold mb-2">{product.name}</h2>
+          <p className="text-xl font-semibold text-gray-800 mb-4">
+            {product.price}
+          </p>
+          <div className="flex items-center gap-x-2">
+            {/* <label htmlFor="quantity" className='mr-2'>Quantity:</label> */}
+            <input
+              type="number"
+              id="quantity"
+              className="border-1 border-gray-200 w-16"
+            />
+            <button className="bg-red-600 py-1.5 px-4 rounded hover:bg-red-800 text-white">
+              Add to cart
+            </button>
+          </div>
+          <div className="flex flex-col gap-y-4 mt-4">
+            <p className="flex items-center">
+              <FaCarSide className="mr-1" /> Delivery & Return
+            </p>
+            <p className="flex items-center">
+              <FaQuestion className="mr-1" />
+              Ask a Question
+            </p>
+          </div>
         </div>
       </div>
-      <div className='mt-8'> 
-        <h3 className='text-xl font-bold mb-2'>Product Description</h3>
+      <div className="mt-8">
+        <h3 className="text-xl font-bold mb-2">Product Description</h3>
         <p>Product description will goes here</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;

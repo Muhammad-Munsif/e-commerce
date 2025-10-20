5786442dac59d0be467fae9eb1bf9ca68d6ac03b
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-20 11:36:38 +0500

    To create a Login form component

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Navbar />
         <main className="">
           <Routes>
-            <Route path="/*" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/cart" element={<Cart />} />
             <Route
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* To add a provider it will make our code is eacy */}
     <Provider store={store}>
-      this is ma first parent Components
+      {/* this is parent Components */}
       <App />
     </Provider>
   </React.StrictMode>

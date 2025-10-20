63c0b935f0b3b885cc4888b9db0225fe9a348f8e
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-20 15:10:56 +0500

    Changes in ProductCard and Register component

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -14,28 +14,28 @@ const ProductCard = ({ product }) => {
   };
   return (
     <Link to={`/product/${product.id}`}>
-    <div className="bg-white shadow p-4 rounded relative transform transition-transform duration-300">
-      <img
-        src={product.image}
-        alt={product.name}
-        className="w-full h-48 object-contain mb-4"
-      />
-      <h3 className="text-lg font-semibold">{product.name}</h3>
-      <p className="text-gray-500">${product.price}</p>
-      <div className="flex items-center mt-2 text-yellow-500">
-        <FaStar />
-        <FaStar />
-        <FaStar />
-        <FaStar />
+      <div className="bg-white shadow p-4 rounded relative transform transition-transform duration-300">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="w-full h-48 object-contain mb-4"
+        />
+        <h3 className="text-lg font-semibold">{product.name}</h3>
+        <p className="text-gray-500">${product.price}</p>
+        <div className="flex items-center mt-2 text-yellow-500">
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+        </div>
+        <div
+          className="absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600 group text-white text-sm rounded-full hover:w-24 hover:bg-red-700 transition-all duration-500 cursor-pointer "
+          onClick={(e) => handleAddToCart(e, product)}
+        >
+          <span className="group-hover:hidden">+</span>
+          <span className="hidden group-hover:block">Add to cart</span>
+        </div>
       </div>
-      <div
-        className="absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600 group text-white text-sm rounded-full hover:w-24 hover:bg-red-700 transition-all duration-500 cursor-pointer "
-        onClick={(e) => handleAddToCart(e, product)}
-      >
-        <span className="group-hover:hidden">+</span>
-        <span className="hidden group-hover:block">Add to cart</span>
-      </div>
-    </div>
     </Link>
   );
 };

ca49a52a24f757783347534f7d11e248579bc45f
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-20 15:13:18 +0500

    Changes in Login component

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,35 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const Login = ({openSignUp}) => {
+const Login = ({ openSignUp }) => {
   return (
     <div>
-      <h2 className='text-2xl font-bold mb-4'>Login</h2>
+      <h2 className="text-2xl font-bold mb-4">Login</h2>
       <form>
-            <div className='mb-4'>
-                <label className="block text-gray-700">Email</label>
-                <input type="email" placeholder='Enter your email' className='w-full py-2 px-3 border-1 border-gray-200 rounded' />
-            </div>
-            <div className='mb-4'>
-                <label className="block text-gray-700">Password</label>
-                <input type="password" placeholder='Enter your Password' className='w-full py-2 px-3 border-1 border-gray-200 rounded'/>
-            </div>
-            <div className='mb-4 flex items-center justify-between'>
-                <label className="inline-flex items-center">
-                    <input type="checkbox" className='form-checkbox'/>
-                    <span className='ml-2 text-gray-700'>Remember Me</span>
-                </label>
-                <a href="" className='text-red-800'>Forgot Password?</a>
-            </div>
-            <div className='mb-4'>
-                <button type='submit' className='w-full bg-red-600 py-2 text-white'>Login</button>
-            </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="w-full py-2 px-3 border-1 border-gray-200 rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Password</label>
+          <input
+            type="password"
+            placeholder="Enter your Password"
+            className="w-full py-2 px-3 border-1 border-gray-200 rounded"
+          />
+        </div>
+        <div className="mb-4 flex items-center justify-between">
+          <label className="inline-flex items-center">
+            <input type="checkbox" className="form-checkbox" />
+            <span className="ml-2 text-gray-700">Remember Me</span>
+          </label>
+          <a href="" className="text-red-800">
+            Forgot Password?
+          </a>
+        </div>
+        <div className="mb-4">
+          <button type="submit" className="w-full bg-red-600 py-2 text-white">
+            Login
+          </button>
+        </div>
       </form>
-      <div className='text-center'>
-        <span className='text-gray-700'>Don't have an Account?</span>
-        <button className='text-red-800' onClick={openSignUp}>Sign Up</button>
+      <div className="text-center">
+        <span className="text-gray-700">Don't have an Account?</span>
+        <button className="text-red-800" onClick={openSignUp}>
+          Sign Up
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

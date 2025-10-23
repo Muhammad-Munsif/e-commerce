58fbeedf492cfd2f55bbdbbf34330b428d46b702
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-23 21:42:16 +0500

    Few changes in dashboard component to solve issue

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,138 +1,138 @@
-import shoes from './shoes.jpg';
-import watch from './watch.jpg';
-import shirt from './shirt.jpg';
-import gold from './gold.jpg';
-import bluetooth from './bluetooth.jpg';
-import jewellary from './jewellary.jpg';
+import shoes from "./shoes.jpg";
+import watch from "./watch.jpg";
+import shirt from "./shirt.jpg";
+import gold from "./gold.jpg";
+import bluetooth from "./bluetooth.jpg";
+import jewellary from "./jewellary.jpg";
 
 export const Categories = [
-    "Electronics",
-    "Fashion",
-    "Home & Kitchen",
-    "Beauty",
-    "Sports",
-    "Automative",
+  "Electronics",
+  "Fashion",
+  "Home & Kitchen",
+  "Beauty",
+  "Sports",
+  "Automative",
 ];
 
 export const mockData = [
-    {
-        id: 1,
-        image: shoes,
-        name: "product 1",
-        price: 23.54
-    },
-    {
-        id: 2,
-        image: watch,
-        name: "product 2",
-        price: 40.54
-    },
-    {
-        id: 3,
-        image: shirt,
-        name: "product 3",
-        price: 35.12
-    },
-    {
-        id: 4,
-        image: jewellary,
-        name: "product 4",
-        price: 54.20
-    },
-    {
-        id: 5,
-        image: gold,
-        name: "product 5",
-        price: 40.54
-    },
-    {
-        id: 6,
-        image: shoes,
-        name: "product 6",
-        price: 2
-    },
-    {
-        id: 7,
-        image: bluetooth,
-        name: "product 7",
-        price: 20.25
-    },
-    {
-        id: 8,
-        image: gold,
-        name: "product 8",
-        price: 75.45
-    },
-    {
-        id: 9,
-        image: shirt,
-        name: "product 9",
-        price: 23.54
-    },
-    {
-        id: 10,
-        image: shoes,
-        name: "product 10",
-        price: 50.54
-    },
-    {
-        id: 11,
-        image: shoes,
-        name: "product 11",
-        price: 23.54
-    },
-    {
-        id: 12,
-        image: watch,
-        name: "product 12",
-        price: 40.54
-    },
-    {
-        id: 13,
-        image: shirt,
-        name: "product 13",
-        price: 35.12
-    },
-    {
-        id: 14,
-        image: jewellary,
-        name: "product 14",
-        price: 54.20
-    },
-    {
-        id: 15,
-        image: gold,
-        name: "product 15",
-        price: 40.54
-    },
-     {
-        id: 16,
-        image: shoes,
-        name: "product 16",
-        price: 2
-    },
-    {
-        id: 17,
-        image: bluetooth,
-        name: "product 17",
-        price: 20.25
-    },
-    {
-        id: 18,
-        image: gold,
-        name: "product 18",
-        price: 75.45
-    },
-    {
-        id: 19,
-        image: shirt,
-        name: "product 19",
-        price: 23.54
-    },
-    {
-        id: 20,
-        image: shoes,
-        name: "product 20",
-        price: 50.54
-    },
+  {
+    id: 1,
+    image: shoes,
+    name: "product 1",
+    price: 23.54,
+  },
+  {
+    id: 2,
+    image: watch,
+    name: "product 2",
+    price: 40.54,
+  },
+  {
+    id: 3,
+    image: shirt,
+    name: "product 3",
+    price: 35.12,
+  },
+  {
+    id: 4,
+    image: jewellary,
+    name: "product 4",
+    price: 54.2,
+  },
+  {
+    id: 5,
+    image: gold,
+    name: "product 5",
+    price: 40.54,
+  },
+  {
+    id: 6,
+    image: shoes,
+    name: "product 6",
+    price: 2,
+  },
+  {
+    id: 7,
+    image: bluetooth,
+    name: "product 7",
+    price: 20.25,
+  },
+  {
+    id: 8,
+    image: gold,
+    name: "product 8",
+    price: 75.45,
+  },
+  {
+    id: 9,
+    image: shirt,
+    name: "product 9",
+    price: 23.54,
+  },
+  {
+    id: 10,
+    image: shoes,
+    name: "product 10",
+    price: 50.54,
+  },
+  {
+    id: 11,
+    image: shoes,
+    name: "product 11",
+    price: 23.54,
+  },
+  {
+    id: 12,
+    image: watch,
+    name: "product 12",
+    price: 40.54,
+  },
+  {
+    id: 13,
+    image: shirt,
+    name: "product 13",
+    price: 35.12,
+  },
+  {
+    id: 14,
+    image: jewellary,
+    name: "product 14",
+    price: 54.2,
+  },
+  {
+    id: 15,
+    image: gold,
+    name: "product 15",
+    price: 40.54,
+  },
+  {
+    id: 16,
+    image: shoes,
+    name: "product 16",
+    price: 2,
+  },
+  {
+    id: 17,
+    image: bluetooth,
+    name: "product 17",
+    price: 20.25,
+  },
+  {
+    id: 18,
+    image: gold,
+    name: "product 18",
+    price: 75.45,
+  },
+  {
+    id: 19,
+    image: shirt,
+    name: "product 19",
+    price: 23.54,
+  },
+  {
+    id: 20,
+    image: shoes,
+    name: "product 20",
+    price: 50.54,
+  },
 ];

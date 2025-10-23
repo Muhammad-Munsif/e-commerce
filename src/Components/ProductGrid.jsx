c8f0b07f3f4f6417d004b9dce4fa690e9b0fdf31
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-23 18:14:03 +0500

    To create multiple components I will designed and with functionality changes in dashboard and navbar component and ProductGrid

--- src/Components/ProductGrid.jsx
+++ src/Components/ProductGrid.jsx
@@ -1,6 +1,5 @@
 // components/ProductGrid.jsx
 import React from "react";
-
 const ProductGrid = ({ children }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4">
@@ -8,5 +7,4 @@ const ProductGrid = ({ children }) => {
     </div>
   );
 };
-
 export default ProductGrid;
